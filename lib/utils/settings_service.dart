2a1aaabc46a41da9897306d4869e96b095b7c4f2
fabianradenta <13522105@mahasiswa.itb.ac.id>
2025-07-30 16:56:33 +0700

    change base url

--- lib/utils/settings_service.dart
+++ lib/utils/settings_service.dart
@@ -2,8 +2,8 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class SettingsService {
   // Default urls
-  static const String _defaultBaseUrl = "http://192.168.17.211:5000/telemetry";
-  static const String _defaultNotificationUrl = "http://192.168.17.211:5000/notification";
+  static const String _defaultBaseUrl = "http://192.168.17.210:6432/api/telemetry";
+  static const String _defaultNotificationUrl = "http://192.168.17.210:5173/api/notification"; // belum diimplement
 
   // Keys for storage
   static const String _baseUrlKey = 'baseUrl';
